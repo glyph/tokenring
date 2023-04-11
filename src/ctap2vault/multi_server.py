68e7531a063816d7a64a0d5fa7ab050c51348dc6
Glyph <glyph@twistedmatrix.com>
2023-04-10 23:19:08 -0700

    wip

--- src/ctap2vault/multi_server.py
+++ src/ctap2vault/multi_server.py
@@ -5,6 +5,27 @@ from .multi_common import address, auth_key, family
 from pyuac import main_requires_admin  # type:ignore[import]
 import sys
 
+
+if sys.platform == 'win32':
+    def _new_handle(self, first=False):
+        import win32security
+        flags = _winapi.PIPE_ACCESS_DUPLEX | _winapi.FILE_FLAG_OVERLAPPED
+        if first:
+            flags |= _winapi.FILE_FLAG_FIRST_PIPE_INSTANCE
+        return _winapi.CreateNamedPipe(
+            self._address, flags,
+            _winapi.PIPE_TYPE_MESSAGE | _winapi.PIPE_READMODE_MESSAGE |
+            _winapi.PIPE_WAIT,
+            _winapi.PIPE_UNLIMITED_INSTANCES, BUFSIZE, BUFSIZE,
+            _winapi.NMPWAIT_WAIT_FOREVER,
+            win32security.ConvertStringSecurityDescriptorToSecurityDescriptor(
+                "D:(A;OICI;GRGW;;;AU)",
+                win32security.SDDL_REVISION_1
+            )
+        )
+    from multiprocessing.connection import PipeListener
+    PipeListener._new_handle = _new_handle
+
 @main_requires_admin(cmdLine=[sys.executable, '-m', 'ctap2vault.multi_server'])
 def main() -> None:
     with Listener(address=address, family=family, authkey=auth_key) as listener:
